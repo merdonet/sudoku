93f1747f35660d443a52cf93802b4b5de9040baa
murat <merdo.net@gmail.com>
2024-01-03 17:33:55 +0100

    install mdi library

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,30 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css';
+import { aliases, mdi } from 'vuetify/lib/iconsets/mdi.mjs';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
 const vuetify = createVuetify({
   components,
-  directives
-})
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi
+    }
+  }
+});
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia()).use(router).use(vuetify)
+app.use(createPinia()).use(router).use(vuetify);
 
-app.mount('#app')
+app.mount('#app');
